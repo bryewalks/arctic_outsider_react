2916ef3f5ee0b3d4569887fcbbf686d698f39e70
bryewalks <bryewalks@gmail.com>
2019-06-24 00:51:27 -0500

    start article index page

--- src/views/articles/ArticlesIndex.js
+++ src/views/articles/ArticlesIndex.js
@@ -1,12 +1,16 @@
 import React from 'react'
-// import axios from 'axios'
+import { Wrapper, Container } from 'components/globals/style'
+import Title from 'components/title'
 
 class ArticlesIndex extends React.Component {
   render() {
     return (
-      <div>
-        <h2>ArticlesIndex</h2>
-      </div>
+      <Wrapper>
+        <Container>
+          <Title title="hello" />
+          <h2>ArticlesIndex</h2>
+        </Container>
+      </Wrapper>
     )
   }
 }
